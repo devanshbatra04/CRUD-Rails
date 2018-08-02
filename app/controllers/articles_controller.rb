752159fbb6fdf2630b249b4ed7712c122db54d6d
Devansh Batra <devanshbatra04@gmail.com>
2018-08-02 15:46:41 +0530

    Showing errors now while creating articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,12 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
 
-    if (@article.save)
+    if @article.save
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
     end
-
 
   end
 
